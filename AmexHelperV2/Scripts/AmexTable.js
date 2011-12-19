7b7d34a84e2917c802a863d914e5a3f36c6d5983
Alan Pies <alan.lee.pies@gmail.com>
2011-12-18 21:42:26 -0800

    jquery working

---
+++ AmexHelperV2/Scripts/AmexTable.js
@@ -0,0 +1,20 @@
+﻿//$("#main #jtable tbody tr").click(function () {
+ //   $(this).toggleClass('hoverHighlight');
+//});
+
+//$(function () { alert($('#jtable tbody tr').length); });
+
+$(document).ready(function () {
+    $('#main #jtable tbody tr').click(function () {
+        $(this).toggleClass('hoverHighlight');
+    });
+});
+
+
+//$(document).ready(function () {
+//    $('#main #jtable tbody tr:even').css('background-color', '#dddddd');
+//});
+
+//$(document).ready(function () {
+//    $('#main h1').css('background-color', '#dddddd');
+//});
